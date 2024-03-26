8db7f075f9e6599d247a6ec265b52f23e2699533
CodeCraftKJ <krzysztofjofficial@gmail.com>
2024-03-26 18:13:38 +0100

    AddedPictures

--- StartFrom.Designer.cs
+++ StartFrom.Designer.cs
@@ -29,13 +29,45 @@ namespace QBankingSystemv2._0
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartFrom));
+            this.pBoxLoading = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxLoading)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBoxLoading
+            // 
+            this.pBoxLoading.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pBoxLoading.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.pBoxLoading.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBoxLoading.Image = ((System.Drawing.Image)(resources.GetObject("pBoxLoading.Image")));
+            this.pBoxLoading.Location = new System.Drawing.Point(0, 0);
+            this.pBoxLoading.Name = "pBoxLoading";
+            this.pBoxLoading.Size = new System.Drawing.Size(800, 450);
+            this.pBoxLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBoxLoading.TabIndex = 0;
+            this.pBoxLoading.TabStop = false;
+            // 
+            // StartFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pBoxLoading);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "StartFrom";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "QPayApp";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxLoading)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pBoxLoading;
     }
 }
 
