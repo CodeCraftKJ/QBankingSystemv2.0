4e641118e4f0d01f9fa41c714b8fd45e66928448
CodeCraftKJ <krzysztofjofficial@gmail.com>
2024-03-27 21:14:11 +0100

    LogInSingIn

--- Forms/WelcomeForm.Designer.cs
+++ Forms/WelcomeForm.Designer.cs
@@ -137,7 +137,7 @@ namespace QBankingSystemv2._0.Forms
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Palatino Linotype", 11.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
             this.label2.Location = new System.Drawing.Point(48, 253);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(379, 26);
@@ -151,7 +151,7 @@ namespace QBankingSystemv2._0.Forms
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Palatino Linotype", 11.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
             this.label1.Location = new System.Drawing.Point(122, 112);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(240, 26);
@@ -163,14 +163,15 @@ namespace QBankingSystemv2._0.Forms
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.BackColor = System.Drawing.Color.Azure;
+            this.button1.Font = new System.Drawing.Font("Stencil", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.ForestGreen;
             this.button1.Location = new System.Drawing.Point(33, 141);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(406, 89);
             this.button1.TabIndex = 5;
             this.button1.Text = "LOG IN";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
@@ -179,7 +180,7 @@ namespace QBankingSystemv2._0.Forms
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Palatino Linotype", 19.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
             this.label3.Location = new System.Drawing.Point(92, 23);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(285, 44);
@@ -192,14 +193,15 @@ namespace QBankingSystemv2._0.Forms
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.BackColor = System.Drawing.Color.MintCream;
+            this.button2.Font = new System.Drawing.Font("Stencil", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.Color.ForestGreen;
             this.button2.Location = new System.Drawing.Point(33, 291);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(406, 89);
             this.button2.TabIndex = 1;
             this.button2.Text = "SIGN IN";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // WelcomeForm
